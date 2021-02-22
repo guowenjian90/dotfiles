34d701b8a62b49e672b955702200cb29a17e331d
Wenjian Guo <wenjian@amazon.com>
2021-02-21 21:38:48 -0800

    change

--- envImprovement/bin/local-override-pkg.sh
+++
@@ -1,7 +0,0 @@
-file=$1
-host=$2
-pkg=$3
-
-ssh $host "mkdir -p /home/wenjian/local-overrides/$pkg; rm -rf /home/wenjian/local-overrides/$pkg/*"
-scp $file $host:/home/wenjian/local-overrides/$pkg/
-ssh $host "cd /home/wenjian/local-overrides/$pkg; tar xvf $file; rm $file"
--- envImprovement/bin/ssh-hostclass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-# fpath=(/home/wenjian/.zshrc $fpath)
-# autoload sshmulti
-HOSTS=`expand-hostclass --recurse --hosts-only $1`
-script_path=`dirname $0`
-HOSTS=${HOSTS:=$*}
-# sshmulti $HOSTS
-sh $script_path/ssh-multi.sh $HOSTS
-
